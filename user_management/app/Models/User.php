8ea540a2e249603bb6043f2f7876c3ba72337891
Matthéo <mvalcke@adin.pro>
2024-01-23 09:58:40 +0100

    Config bdd

--- user_management/app/Models/User.php
+++ user_management/app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable implements JWTSubject {
     {
         return [];
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
--- user_management/database/seeders/RolesTableSeeder.php
+++ user_management/database/seeders/RolesTableSeeder.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\Role;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesTableSeeder extends Seeder
 {
@@ -15,8 +16,19 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
+        // Désactiver les contraintes de clé étrangère
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        // Tronquer la table users
+        User::truncate();
+
+        // Tronquer la table roles
         Role::truncate();
 
+        // Réactiver les contraintes de clé étrangère
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+        // Créer de nouveaux rôles
         Role::create(['name' => 'administrateur']);
         Role::create(['name' => 'enseignant']);
         Role::create(['name' => 'élève']);
