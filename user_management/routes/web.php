36fe8b0dedccd00b5abddca4d7b81d9795ba5201
Matthéo <mvalcke@adin.pro>
2024-01-23 10:18:46 +0100

    Config bdd

--- user_management/routes/web.php
+++ user_management/routes/web.php
@@ -8,6 +8,6 @@ Route::get('/login', [ApiController::class, 'login']);
 Route::get('/user/{idUtilisateur}', [ApiController::class, 'getUserById'])->middleware('jwt.verify');;
 Route::put('/user/{idUtilisateur}', [ApiController::class, 'updateUserById'])->middleware('jwt.verify');;
 Route::delete('/user/{idUtilisateur}', [ApiController::class, 'deleteUserById'])->middleware('jwt.verify');;
-Route::get('/user', [ApiController::class, 'user'])->middleware('jwt.verify');
+Route::get('/user', [ApiController::class, 'user']);
 Route::post('/user', [ApiController::class, 'createUser'])->middleware('jwt.verify');
 Route::get('/logout', [ApiController::class, 'logout'])->middleware('jwt.verify');
