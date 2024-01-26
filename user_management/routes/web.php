5e4d05fd76c3c326e7b1ee78b85b5bbec2dc2ff6
Clement Soltysiak <soltysiak.clement@outlook.fr>
2024-01-26 09:43:17 +0100

    ajout de middlware jwt.verify sur Postuser

--- user_management/routes/web.php
+++ user_management/routes/web.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 Route::get('/login', [ApiController::class, 'login']);
-Route::get('/user/{idUtilisateur}', [ApiController::class, 'getUserById'])->middleware('jwt.verify');;
-Route::put('/user/{idUtilisateur}', [ApiController::class, 'updateUserById'])->middleware('jwt.verify');;
-Route::delete('/user/{idUtilisateur}', [ApiController::class, 'deleteUserById'])->middleware('jwt.verify');;
-Route::get('/user', [ApiController::class, 'user']);
+Route::get('/user/{idUtilisateur}', [ApiController::class, 'getUserById'])->middleware('jwt.verify');
+Route::put('/user/{idUtilisateur}', [ApiController::class, 'updateUserById'])->middleware('jwt.verify');
+Route::delete('/user/{idUtilisateur}', [ApiController::class, 'deleteUserById'])->middleware('jwt.verify');
+Route::get('/user', [ApiController::class, 'user'])->middleware('jwt.verify');
 Route::post('/user', [ApiController::class, 'createUser'])->middleware('jwt.verify');
 Route::get('/logout', [ApiController::class, 'logout'])->middleware('jwt.verify');
 Route::post('/verifyToken', [ApiController::class, 'verifyToken']);
